5f949c93cb81bb7503b543b44ab97e941b5a0c0b
nathan breitsch <nathanbreitsch@gmail.com>
2016-02-13 14:15:52 -0500

    got api working

--- server/app.py
+++ server/app.py
@@ -1,39 +1,29 @@
 import falcon
 import json
+from stats import compute_qb_stats, get_actions, group_by_athlete
 
 api = application = falcon.API()
 
-class DataContext():
 
-    def __init__(self):
-        with open('../data/games.json', 'r') as f:
-            self.games = json.load(f)
+with open('../data/games.json', 'r') as f:
+    games = json.load(f)
 
-    # some shit i copied and pasted off of s.o.
-    # http://stackoverflow.com/questions/8477550/flattening-a-list-of-dicts-of-lists-of-dicts-etc-of-unknown-depth-in-python-n
-    def flatten(self, structure, key="", path="", flattened=None):
-        if flattened is None:
-            flattened = {}
-        if type(structure) not in(dict, list):
-            flattened[((path + "_") if path else "") + key] = structure
-        elif isinstance(structure, list):
-            for i, item in enumerate(structure):
-                flatten(item, "%d" % i, path + "_" + key, flattened)
-        else:
-            for new_key, value in structure.items():
-                flatten(value, new_key, path + "_" + key, flattened)
-        return flattened
 
-dc = DataContext()
 
-class GameResource(object):
+
+class QuarterbackStatsResource(object):
 
     def on_get(self, req, resp):
-        resp.body = json.dumps(dc.games)
+        actions = get_actions(games, {'action_type': 'quarterback'})
+        action_groups = group_by_athlete(actions)
+        qb_stats = [{
+            'athlete': g['athlete'],
+            'stats': compute_qb_stats(g['actions'])
+        } for g in action_groups]
+
+        resp.body = json.dumps({'quarterbacks': qb_stats})
         resp.status = falcon.HTTP_200
 
-        json.loads
 
-
-resource = GameResource()
-api.add_route('/hello', resource)
+quarterback_resource = QuarterbackStatsResource()
+api.add_route('/quarterback/stats', quarterback_resource)
--- server/stats.py
+++ server/stats.py
@@ -1,1 +1,110 @@
-import collections
+from collections import namedtuple
+from itertools import groupby
+
+
+"""
+input: list of actions
+output: list of athlete_groups
+"""
+def group_by_athlete(actions):
+    #one must sort before grouping
+    actions_sorted = sorted(actions, key = lambda a: a['athlete'])
+    actions_grouped = groupby(actions_sorted, lambda a: a['athlete'])
+    action_groups = [{'athlete': key, 'actions': list(group)} for key, group in actions_grouped]
+    return action_groups
+
+def safe_parse_int(text):
+    if text.isdigit():
+        return int(text)
+    else:
+        return 0
+
+def parse_dropback(text):
+    if 'SG' in text or 'UC' in text:
+        return 1
+    else:
+        return 0
+
+
+def compute_qb_stats(actions):
+    print(actions[0])
+    drop_back_count = len([1 for a in actions
+                           if a['observations']['SG/UC'] in ['SG', 'UC']])
+
+    attempts = sum([1 for a in actions
+                       if a['observations']['Att'] == '1'])
+
+    completions = completions = sum([1 for a in actions
+                       if a['observations']['Comp'] == '1'])
+
+    total_yards = sum(map(
+        lambda a: safe_parse_int(a['observations']['Tyds']),
+        actions
+    ))
+
+    air_yards = sum(map(
+        lambda a: safe_parse_int(a['observations']['AirYds']),
+        actions
+    ))
+
+    pressured_count = sum([1 for a in actions
+                       if a['observations']['Press'] == '1'])
+
+    touchdown_count = sum([1 for a in actions
+                       if a['observations']['TD'] == '1'])
+
+    interception_count = sum([1 for a in actions
+                       if a['observations']['Int'] == '1'])
+
+    throwaway_count = sum([1 for a in actions
+                       if a['observations']['TA'] == '1'])
+
+    sack_count = sum([1 for a in actions
+                       if a['observations']['Sk'] == '1'])
+
+    hit_count = sum([1 for a in actions
+                       if a['observations']['Ht'] == '1'])
+
+    hurried_count = sum([1 for a in actions
+                       if a['observations']['Hur'] == '1'])
+
+    action_count = len(actions)
+
+
+    completion_rate = round(completions / attempts, 3) if attempts > 0 else 0
+
+
+    air_yards_per_attempt = round(air_yards / attempts, 3) if attempts > 0 else 0
+
+
+
+    return {
+        'dropback_count': drop_back_count,
+        'attempts': attempts,
+        'completions': completions,
+        'completion_rate': completion_rate,
+        'total_yards': total_yards,
+        'air_yards': air_yards,
+        'air_yards_per_attempt': air_yards_per_attempt,
+        'pressured_count': pressured_count,
+        'touchdown_count': touchdown_count,
+        'interception_count': interception_count,
+        'throwaway_count': throwaway_count,
+        'sack_count': sack_count,
+        'hit_count': hit_count,
+        'hurried_count': hurried_count
+    }
+
+def get_actions(games, game_filter):
+    action_type = game_filter['action_type']
+
+    actions = []
+    for game in games:
+        for quarter in game['quarters']:
+            for play in quarter['plays']:
+                for action in play['actions']:
+                    if 'athlete' in action:
+                        if action['action_type'] == action_type:
+                            action['athlete'] = action['athlete']['$oid']
+                            actions.append(action)
+    return actions
